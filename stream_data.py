ba5a0de6c7c20fc213d67ba1af380fcfd844fa78
Brady Berg <bergbrae@msu.edu>
2023-10-18 18:31:09 -0400

    clarify connection string

--- stream_data.py
+++ stream_data.py
@@ -2,7 +2,7 @@ from pymavlink import mavutil
 import datetime
 import json
 
-# Connect to the vehicle (assuming you're using UDP here, but adjust as necessary)
+# Connect to the vehicle
 baud = 57600
 mav = mavutil.mavlink_connection(
     "/dev/cu.usbserial-D30BQ4ZZ",
@@ -17,7 +17,7 @@ messages = []
 print("Heartbeat received")
 for i in range(1000):
     # Wait for a new message
-    msg = mav.recv_match(type=["GLOBAL_POSITION_INT", "ATTITUDE"], blocking=True)
+    msg = mav.recv_match(type=["GLOBAL_POSITION_INT", "ATTITUDE"], blocking=True) # see docs on blocking=True
     messages.append(msg.to_dict())
     print(i)
 
@@ -27,7 +27,11 @@ with open(f"data_{dt_string}.json", "w") as f:
     f.write(json_data)
 
 # possible baud rates: 2400, 4800, 9600, 19200, 38400, 57600, 115200, 230400, 460800, 500000, 921600
+#   For Atlas's telemetry radio, 57600 is the baud rate
+
+# to find the connection string on mac:
+#   ls /dev/*usb*
 # possbile connection strign prefixes: cu., tty.
 
-# ls /dev/*usb*:
-# /dev/cu.usbserial-D30BQ4ZZ      /dev/tty.usbserial-D30BQ4ZZ
+# on Brady's mac:
+# /dev/cu.usbserial-D30BQ4ZZ
