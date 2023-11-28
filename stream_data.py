941a693d208e48b505cfccf2d8a7b8ef9462b214
Brady Berg <bergbrae@msu.edu>
2023-11-27 20:11:03 -0500

    increase telem rate

--- stream_data.py
+++ stream_data.py
@@ -5,26 +5,42 @@ import json
 # Connect to the vehicle
 baud = 57600
 mav = mavutil.mavlink_connection(
-    "/dev/cu.usbserial-D30F0LHK",
+    "/dev/cu.usbmodem21201",
     baud=baud,
 )  # Adjust the connection string as needed
 print(f"baud is {baud}")
 print("Waiting for heartbeat")
 mav.wait_heartbeat()
 
+# Request data streams
+data_streams = ["ALL"]#, "GLOBAL_POSITION_INT", "ATTITUDE"]
+stream_rate = 200  # Adjust the stream rate as needed (messages per second)
+
+for data_stream in data_streams:
+    mav.mav.request_data_stream_send(
+        mav.target_system, mav.target_component,
+        mavutil.mavlink.MAV_DATA_STREAM_ALL if data_stream == 'ALL' else getattr(mavutil.mavlink, f'MAV_DATA_STREAM_{data_stream}'),
+        stream_rate,
+        1  # 1 to start sending, 0 to stop
+    )
+
 messages = []
-
-print("Heartbeat received")
-for i in range(1000):
-    # Wait for a new message
-    msg = mav.recv_match(type=["GLOBAL_POSITION_INT", "ATTITUDE"], blocking=True) # see docs on blocking=True
-    messages.append(msg.to_dict())
-    print(i)
-
-dt_string = datetime.datetime.now().strftime("%d%m%Y-%H:%M:%S")
-json_data = json.dumps(messages)
-with open(f"data_{dt_string}.json", "w") as f:
-    f.write(json_data)
+data_streams = ["GLOBAL_POSITION_INT", "ATTITUDE"]
+print("Data streams requested")
+try:
+    for i in range(3000):
+        # Wait for a new message
+        msg = mav.recv_match(type=data_streams, blocking=True)  # see docs on blocking=True
+        messages.append(msg.to_dict())
+        print(i)
+except KeyboardInterrupt:
+    pass
+finally:
+    dt_string = datetime.datetime.now().strftime("%d%m%Y-%H:%M:%S")
+    json_data = json.dumps(messages)
+    with open(f"data_{dt_string}.json", "w") as f:
+        f.write(json_data)
+    mav.close()
 
 # possible baud rates: 2400, 4800, 9600, 19200, 38400, 57600, 115200, 230400, 460800, 500000, 921600
 #   For Atlas's telemetry radio, 57600 is the baud rate
