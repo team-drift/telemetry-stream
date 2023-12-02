9b9d71d2c26b4fe1a7ff94f252bd488bdb6a7da1
Brady Berg <bergbrae@msu.edu>
2023-12-02 11:45:37 -0500

    update to change streaming rates

--- stream_data.py
+++ stream_data.py
@@ -1,56 +1,67 @@
 from pymavlink import mavutil
 import datetime
 import json
+import os
+import time
 
-# Connect to the vehicle
-baud = 57600
-mav = mavutil.mavlink_connection(
-    "/dev/cu.usbmodem21201",
-    baud=baud,
-)  # Adjust the connection string as needed
-print(f"baud is {baud}")
-print("Waiting for heartbeat")
-mav.wait_heartbeat()
+# https://github.com/ArduPilot/pymavlink/blob/master/tools/mavtelemetry_datarates.py
+# ESTIMATOR_STATUS -> pos_horiz_accuracy
 
-# Request data streams
-data_streams = ["ALL"]#, "GLOBAL_POSITION_INT", "ATTITUDE"]
-stream_rate = 200  # Adjust the stream rate as needed (messages per second)
+def connect(connection_string, baud=57600):
+    # Connect to the vehicle
+    mav = mavutil.mavlink_connection(
+        connection_string,
+        baud=baud,
+    )
+    print("Waiting for heartbeat")
+    mav.wait_heartbeat()
+    return mav
 
-for data_stream in data_streams:
+def request_data_stream(mav, stream_id=mavutil.mavlink.MAV_DATA_STREAM_ALL, stream_rate=200):
     mav.mav.request_data_stream_send(
-        mav.target_system, mav.target_component,
-        mavutil.mavlink.MAV_DATA_STREAM_ALL if data_stream == 'ALL' else getattr(mavutil.mavlink, f'MAV_DATA_STREAM_{data_stream}'),
+        mav.target_system,
+        mav.target_component,
+        stream_id,
         stream_rate,
-        1  # 1 to start sending, 0 to stop
+        1,  # 1 to start sending, 0 to stop
     )
 
-messages = []
-data_streams = ["GLOBAL_POSITION_INT", "ATTITUDE"]
-print("Data streams requested")
-try:
-    for i in range(3000):
-        # Wait for a new message
-        msg = mav.recv_match(type=data_streams, blocking=True)  # see docs on blocking=True
-        messages.append(msg.to_dict())
-        print(i)
-except KeyboardInterrupt:
-    pass
-finally:
-    dt_string = datetime.datetime.now().strftime("%d%m%Y-%H:%M:%S")
-    json_data = json.dumps(messages)
-    with open(f"data_{dt_string}.json", "w") as f:
-        f.write(json_data)
-    mav.close()
+def record_data(mav):
+    unique_mavpackettypes = set()
+    messages = []
+    try:
+        i = 0
+        start_time = time.time()
+        while True:
+            # Wait for a new message
+            msg = mav.recv_match(blocking=True).to_dict()
+            messages.append(msg)
+            unique_mavpackettypes.add(msg.get('mavpackettype'))
+            i += 1
+            if i % 100 == 0:
+                end_time = time.time()
+                elapsed_time = end_time - start_time
+                average_hz = 100 / elapsed_time if elapsed_time > 0 else 0
+                print(f"{i} messages received, Average Hz: {average_hz:.2f}, Number of unique mavpackettypes: {len(unique_mavpackettypes)}")
+                start_time = time.time()
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        dt_string = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+        json_data = json.dumps(messages)
+        if not os.path.exists("./data/"):
+            os.makedirs("./data/")
+        with open(f"./data/{dt_string}.json", "w") as f:
+            f.write(json_data)
+        mav.close()
 
-# possible baud rates: 2400, 4800, 9600, 19200, 38400, 57600, 115200, 230400, 460800, 500000, 921600
-#   For Atlas's telemetry radio, 57600 is the baud rate
+if __name__ == "__main__":
+    connection_string = "/dev/cu.usbserial-D30F0LHK"
+    mav = connect(connection_string)
+    request_data_stream(mav, stream_rate=3)
+    request_data_stream(mav, stream_id=mavutil.mavlink.MAV_DATA_STREAM_POSITION, stream_rate=50) # position
+    request_data_stream(mav, stream_id=mavutil.mavlink.MAV_DATA_STREAM_EXTRA1, stream_rate=50) # attitude
 
-# to find the connection string on mac:
-#   ls /dev/*usb*
-# possbile connection strign prefixes: cu., tty.
-
-# on mac for Atlas radio:
-# /dev/cu.usbserial-D30BQ4ZZ
-
-# on mac for Atlas 2 radio:
-# /dev/cu.usbserial-D30F0LHK
+    record_data(mav)
+    # lower the stream rate
+    request_data_stream(mav, stream_rate=3)
